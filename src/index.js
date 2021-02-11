c3173baba60900e91b6532cd2a622570861e08ac
Daria Terenteva <thevioletmaniac@gmail.com>
2021-02-11 18:18:50 +0600

    Fix router base

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/weather-app">
     <App />
   </Router>,
   document.getElementById('app')
